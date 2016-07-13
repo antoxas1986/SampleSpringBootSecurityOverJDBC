c96b2d8971401ffa7a3296a297ca7a14be0a59dc
Anton Kuznetsov <akuznetsov@catalystdevworks.com>
2016-07-13 13:19:14 -0400

    logging through log4j to file

--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -2,6 +2,8 @@ package com.example.controllers;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+	private Logger logger =  LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
+		logger.info("Serve index page");
 		return "index";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
+		logger.info("Serve login page");
 		return "login";
 	}
 
@@ -24,19 +30,23 @@ public class HomeController {
 	@RequestMapping(value = "/after", method = RequestMethod.GET)
 	public String after(Principal principal) {
 		if (principal.getName().equals("admin")) {
+			logger.info("Serve admin page {}", principal.getName());
 			return "admin";
 		} else {
+			logger.info("Serve user page {}", principal.getName());
 			return "user";
 		}
 	}
 
 	@RequestMapping(value = "/403", method = RequestMethod.GET)
 	public String fourOthree() {
+		logger.info("Serve error 403 page");
 		return "403";
 	}
 
 	@RequestMapping(value = "/error", method = RequestMethod.GET)
 	public String error() {
+		logger.info("Serve error page");
 		return "error";
 	}
 
